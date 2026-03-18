bf3d3ed3d738636bf683f7931c65762b65791068
Kirath Kumar Sharma <144979032+KIRATH25@users.noreply.github.com>
2026-03-18 14:39:59 +0530

    Question-10
    Implement a program to count and display elements greater than 10 from user input.

---
+++ Arrays/1. Traversal Pattern/1. Basic iteration/_9_Greater_10.java
@@ -0,0 +1,27 @@
+// Question 9
+// Description
+// Count how many elements are greater than 10.
+// Sample Input
+// 6
+// 5 12 18 7 3 20
+// Sample Output
+// 3
+// Hint
+// Use condition arr[i] > 10 .
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]>10){
+                System.out.print(arr[i]+" ");    
+            }
+        }
+    }
+}
