b09f00a12131d11faa154700374c21480b3f6c78
Kirath Kumar Sharma <144979032+KIRATH25@users.noreply.github.com>
2026-03-19 23:12:36 +0530

    Question-4
    This program prints the indexes of odd numbers in an array while traversing from the end.

---
+++ Arrays/1. Traversal Pattern/2. Reverse Traversal/_4_Odd_Number_Indices.java
@@ -0,0 +1,28 @@
+// Question 5
+// Description
+// Print the indexes of odd numbers while traversing from the end.
+// Sample Input
+// 5
+// 2 7 4 9 6
+// Sample Output
+// 3 1
+
+// Hint
+// Check arr[i] % 2 != 0 .
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=n-1;i>=0;i--){
+            if(arr[i]%2!=0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
