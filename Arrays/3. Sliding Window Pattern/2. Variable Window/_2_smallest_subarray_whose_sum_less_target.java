c6e4ac7ddbafc4acbb091583da9b8a3cdef14378
Kirath Kumar Sharma <144979032+KIRATH25@users.noreply.github.com>
2026-04-05 23:31:58 +0530

    Question-2

---
+++ Arrays/3. Sliding Window Pattern/2. Variable Window/_2_smallest_subarray_whose_sum_less_target.java
@@ -0,0 +1,35 @@
+// Question 2
+// Description
+// Find the smallest subarray whose sum ≥ target.
+// Sample Input
+// 6
+// 2 3 1 2 4 3
+// 7
+// Sample Output
+// 2
+// Hint
+// Shrink window once sum reaches target.
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int k=sc.nextInt();
+        int l=0,r=0,minlen=Integer.MAX_VALUE,sum=0;
+         while(r<n){
+            sum+=arr[r];
+            while(sum>=k){
+                minlen=Math.min(minlen,r-l+1);
+                sum-=arr[l++];
+            }
+            r++;
+        }
+        System.out.println(minlen);
+    }
+}
