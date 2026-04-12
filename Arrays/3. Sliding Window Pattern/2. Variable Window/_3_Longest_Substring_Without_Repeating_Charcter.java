b7f2bf6d85798bb44931d4597a6148e330a212d0
Kirath Kumar Sharma <144979032+KIRATH25@users.noreply.github.com>
2026-04-12 20:40:38 +0530

    Question-3

---
+++ Arrays/3. Sliding Window Pattern/2. Variable Window/_3_Longest_Substring_Without_Repeating_Charcter.java
@@ -0,0 +1,35 @@
+// Question 3
+// Description
+// Find the longest substring without repeating characters.
+// Sample Input
+// abcabcbb
+// Sample Output
+// 3
+// Hint
+// Use a set to track characters in the current window.
+
+import java.util.*;
+
+public class _1_without_repeat_char {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        int l=0,r=0,maxlen=Integer.MIN_VALUE;
+        HashSet<Character> set=new HashSet<>();
+         while (r < s.length()) {
+            char ch = s.charAt(r);
+            if (!set.contains(ch)) {
+                set.add(ch);
+                maxlen = Math.max(maxlen, r - l + 1);
+                r++;
+            }
+            else {
+                set.remove(s.charAt(l));
+                l++;
+            }
+        }
+        System.out.println(maxlen);
+    }
+}
+
+Q
