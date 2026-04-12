3c93e649e8f834bb5fe3c0399f51e1ed1c7cad2a
Kirath Kumar Sharma <144979032+KIRATH25@users.noreply.github.com>
2026-04-12 20:52:54 +0530

    Question-6

---
+++ Arrays/3. Sliding Window Pattern/2. Variable Window/_7_Longest_SubString_with_K_unique_char.java
@@ -0,0 +1,36 @@
+// Find the longest substring with exactly k unique characters.
+// Sample Input
+// aabacbebebe
+// 3
+// Sample Output
+// 7
+// Hint
+// Expand until distinct characters exceed k.
+
+import java.util.*;
+
+public class _4_longest_subarray_with_sum_k {
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            String s=sc.next();
+            int k=sc.nextInt();
+            HashMap<Character,Integer> map=new HashMap<>();
+            int l=0,r=0,maxlen=0;
+            while(r<s.length()){
+                char ch=s.charAt(r);
+                map.put(ch,map.getOrDefault(ch,0)+1);
+                if(map.size()>k){
+                    map.put(s.charAt(l),map.get(s.charAt(l))-1);
+                    if(map.get(s.charAt(l))==0){
+                        map.remove(s.charAt(l));
+                    }
+                    l++;
+                }
+                if(map.size()==k){
+                    maxlen=Math.max(maxlen,r-l+1);
+                }
+                r++;
+            }
+            System.out.println(maxlen);
+    }
+}
