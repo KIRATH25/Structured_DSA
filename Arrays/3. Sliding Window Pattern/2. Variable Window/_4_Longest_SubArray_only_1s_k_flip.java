34ce631921289b251b4755a91b5d5971ec14285f
Kirath Kumar Sharma <144979032+KIRATH25@users.noreply.github.com>
2026-04-12 20:44:29 +0530

    Question-4

---
+++ Arrays/3. Sliding Window Pattern/2. Variable Window/_4_Longest_SubArray_only_1s_k_flip.java
@@ -0,0 +1,37 @@
+// Find the longest subarray containing only 1s if you can flip at most k zero
+// Sample Input
+// 7
+// 1 1 0 0 1 1 1
+// 1
+// Sample Output
+// 4
+// Hint
+// Track number of zeros in the window.
+
+import java.util.Scanner;
+
+public class _2_max_1s {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int k=sc.nextInt();
+        int left = 0, zeros = 0, maxLen = 0;
+        for (int right = 0; right < arr.length; right++) {
+            if (arr[right] == 0) {
+                zeros++;
+            }
+            while (zeros > k) {
+                if (arr[left] == 0) {
+                    zeros--;
+                }
+                left++;
+            }
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        System.out.println(maxLen);
+    }
+}
