56619f564fe3ac8a020b324dac8606728b5cbfe9
Kirath Kumar Sharma <144979032+KIRATH25@users.noreply.github.com>
2026-04-13 22:33:23 +0530

    Question-8

---
+++ Arrays/3. Sliding Window Pattern/2. Variable Window/_8_Max_Vowels_in_substring.java
@@ -0,0 +1,39 @@
+// Question 8
+// Description
+// Find the maximum number of vowels in any substring.
+// Sample Input
+// abciiidef
+// 3
+// Sample Output
+// 3
+// Hint
+// Track vowels in current window.
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int k = sc.nextInt();
+        int count = 0;
+        for(int i = 0; i < k; i++){
+            if(isVowel(s.charAt(i))){
+                count++;
+            }
+        }
+        int maxCount = count;
+        for(int i = k; i < s.length(); i++){
+            if(isVowel(s.charAt(i))){
+                count++;
+            }
+            if(isVowel(s.charAt(i - k))){
+                count--;
+            }
+            maxCount = Math.max(maxCount, count);
+        }
+        System.out.println(maxCount);
+    }
+    static boolean isVowel(char ch){
+        return ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u';
+    }
+}
